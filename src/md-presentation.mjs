c6dce93926c121826b26c1227f52c12702d0e1b7
Erik Gillespie <erik.gillespie@gmail.com>
2021-10-29 19:26:28 -0400

    Split markdown into sections and render separately to create slides

--- src/md-presentation.mjs
+++ src/md-presentation.mjs
@@ -1,39 +1,16 @@
 import marked from 'https://cdn.jsdelivr.net/npm/marked@3.0.8/lib/marked.esm.js'
 import syncAttribute from './helpers/sync-attribute.mjs'
 import invokeOnChangeAttribute from './helpers/invoke-on-change-attribute.mjs'
+import splitMarkdownSections from './helpers/split-markdown-sections.mjs'
 
-const renderer = {
-  heading (text, level) {
-    const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
-    const sectionStart = level <= 2 ? '<section class="slide">' : ''
-    const prevSectionClose = level === 2 ? '</section>' : ''
-    const heading = /* html */ `
-      <h${level}>
-        <a name="${escapedText}" class="anchor" href="#${escapedText}">
-          <span class="header-link"></span>
-        </a>
-        ${text}
-      </h${level}>
-    `
-    return `${prevSectionClose}${sectionStart}${heading}`
-  }
-}
-
-marked.use({ renderer })
-
-const html = /* html */ `
-  <div id="slides"></div>
-`
+const html = /* html */ `<div id="presentation"></div>`
 
 export default class MdPresentation extends HTMLElement {
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.innerHTML = html
-    this._slides = this.shadowRoot.getElementById('slides')
-  }
-
-  connectedCallback () {
+    this._slides = this.shadowRoot.getElementById('presentation')
   }
 
   get src () {
@@ -55,8 +32,14 @@ export default class MdPresentation extends HTMLElement {
   async onChangeSrc (newSrc) {
     const response = await fetch(newSrc)
     const markdown = await response.text()
-    const html = marked.parse(markdown) + '</section>'
-    this._slides.innerHTML = html
+    const sections = splitMarkdownSections(markdown)
+      .map((md, index) => /* html */ `
+        <section id="slide-${index}" class="slide">
+          ${marked.parse(md)}
+        </section>
+      `)
+      .join('')
+    this._slides.innerHTML = sections
   }
 }
 
